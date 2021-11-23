3c8d9a8244f01fb21e73f72ddd1e5adc0bace66e
Amit Vashist <amitvashist7@outlook.com>
2021-11-23 02:25:09 +0000

    10-Solutions

--- 11-Expression/expression.tf
+++ 11-Expression/expression.tf
@@ -88,3 +88,8 @@ output "PublicIP_East_Backend" {
   value = aws_instance.east_backend.*.public_ip
 }
 
+
+
+output "tag_name" {
+  value = concat(aws_instance.east_backend.*.tags.Name, aws_instance.east_frontend.*.tags.Name)
+}
