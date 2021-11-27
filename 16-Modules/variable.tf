569252ea7ffe9c9bac8516e6e2718b9bec8653a4
Amit Vashist <amitvashist7@outlook.com>
2021-11-27 02:55:19 +0000

    16-Modules

---
+++ 16-Modules/variable.tf
@@ -0,0 +1,15 @@
+variable "frontend_instance_count" {
+  default = "2"
+}
+
+
+
+
+variable "backend_instance_count" {
+  default = "2"
+}
+
+
+variable "backend_region" {
+  default = "us-east-2"
+}
